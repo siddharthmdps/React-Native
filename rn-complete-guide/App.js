a45f5a8d703bb976238afefe225b4435bdd9a862
Siddharth SIngh <siddharthmdps@gmail.com>
2020-01-24 18:21:06 +0530

    complete guide setup

---
+++ rn-complete-guide/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ padding: 50 }}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Course Goal" style={{width: '80%', borderBottomColor: 'black', borderBottomWidth: 1, padding: 10 }}/>
+        <Button title="ADD"/>
+      </View>
+      <View></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
