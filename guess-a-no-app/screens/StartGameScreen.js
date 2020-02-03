9073702a94364d5c5317f6e7036abd551a07866f
Siddharth SIngh <siddharthmdps@gmail.com>
2020-02-03 12:00:00 +0530

    react native card game

---
+++ guess-a-no-app/screens/StartGameScreen.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+import Card from '../components/Card';
+import Colors from '../constants/colors';
+import Input from '../components/Input';
+
+const StartGameScreen = props => {
+    return(
+        <View style={styles.screen}>
+            <Text style={styles.title}>Start a New Game</Text>
+            <Card style = {styles.inputContainer}>
+                <Text>Select a Number: </Text>
+                <Input />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}><Button color={Colors.accent} title="Reset" onPress={() => {}}/></View>
+                    <View style={styles.button}><Button color={Colors.primary} title="Confirm" onPress={() => {}}/></View>
+                </View>
+            </Card>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    button: {
+        width: 100
+    }
+});
+
+export default StartGameScreen;
